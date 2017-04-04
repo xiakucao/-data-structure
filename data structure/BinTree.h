6a45a22dee6d405e6a3253abdc6cc88e652fdae8
xiakucao <fangchong_1@163.com>
2017-04-04 20:51:45 +0800

    Update BinTree.h

--- data structure/BinTree.h
+++ data structure/BinTree.h
@@ -10,13 +10,13 @@ typedef struct Node {
 class BinTree {
 public:
 	BinTree();
-	void CreatBinTree(pNode &root);
-	pNode find(pNode root,T &data) const;
-	void preorder(pNode &t) const;
-	void midorder(pNode &t) const;
-	void postorder(pNode &t) const;
-	void levelorder(pNode &t) const;
-	int high(pNode &t) const;
+	void CreatBinTree(pNode root);
+	pNode find(pNode root,T data) const;
+	void preorder(pNode t) const;
+	void midorder(pNode t) const;
+	void postorder(pNode t) const;
+	void levelorder(pNode t) const;
+	int high(pNode t) const;
 	~BinTree();
 private:
 	pNode root;
